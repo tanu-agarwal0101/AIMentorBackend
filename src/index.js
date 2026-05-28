99f49ad05ab0e4d761767e683e02904e6767820f
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2026-05-28 22:52:51 +0530

    chore: update package configurations and app entrypoint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import dotenv from "dotenv"
 dotenv.config();
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { authRoutes, chatRoutes, userRoutes } from "./routes/index.js";
+import codyRoutes from "./routes/codyRoutes.js";
+import interviewReviewRoutes from "./routes/interviewReviewRoutes.js";
+import problemRoutes from "./routes/problemRoutes.js";
+import submissionRoutes from "./routes/submissionRoutes.js";
+import feedbackRoutes from "./routes/feedbackRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import statsRoutes from "./routes/statsRoutes.js";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "./utils/passport.js";
 import http from "http"
 import { Server } from "socket.io"
-import jwt from 'jsonwebtoken';
 import { registerChatHandlers } from "./utils/chatSocket.js";
 import { socketAuth } from "./middlewares/authMiddleware.js";
 
@@ -32,6 +38,13 @@ app.use(
 app.use('/api/auth', authRoutes)
 // app.use("/api/chat", chatRoutes)
 app.use('/api/user', userRoutes)
+app.use('/api/cody', codyRoutes)
+app.use('/api/interview-review', interviewReviewRoutes)
+app.use('/api/problems', problemRoutes)
+app.use('/api/submissions', submissionRoutes)
+app.use('/api/feedback', feedbackRoutes)
+app.use('/api/events', eventRoutes)
+app.use('/api/stats', statsRoutes)
 
 app.use(passport.initialize())
 app.use(passport.session())
